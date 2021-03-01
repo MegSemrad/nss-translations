76df305493b7940bd18689bcd1af6c53c10d7485
Meghan Semrad <mlsemrad753@gmail.com>
2021-03-01 15:36:44 -0600

    finished rolling dice exercise

---
+++ rolling-dice/js/rollingDice/main.js
@@ -0,0 +1,34 @@
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+debugger
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const runFunctionTenTimes = () => {
+    for (let i = 0; i < 10; i++) {
+        dieFunction(i)
+    }
+}
+
+const dieFunction = () => {
+    const dieOne = getRandomInt(1, 6);
+    const dieTwo = getRandomInt(1, 6);
+
+    const addedDieValue = dieOne + dieTwo
+
+    const message = `${dieOne} + ${dieTwo} === ${addedDieValue}`
+
+    if (dieOne === dieTwo) {
+        console.log(`${dieOne} + ${dieTwo} === ${addedDieValue} DOUBLES!`)
+    }
+    else {
+        console.log(message)
+    }
+};
+
+
+
+runFunctionTenTimes()
