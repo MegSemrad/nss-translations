2252036dee8e1db61557c0f3c570ae49a6890c28
Meghan Semrad <mlsemrad753@gmail.com>
2021-03-01 09:39:17 -0600

    got most names correct

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,15 @@
-// Put your code here
+const contentElement = document.querySelector(".placeNames");
+const theNameContentElement = document.querySelector(".thePlaceNames");
+
+const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+
+for (let name of names) {
+    contentElement.innerHTML += `${name}<br/>`
+};
+
+
+const namesThatStartsWithThe = names.filter(n => n.startsWith("The"))
+const namesWithRemovedComma = namesThatStartsWithThe.join(" ")
+theNameContentElement.innerHTML += `${namesWithRemovedComma}<br/>`
