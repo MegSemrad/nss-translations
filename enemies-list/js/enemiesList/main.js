acc51066fcfb437fbd941847cbfe9e1d1e35f6d4
Meghan Semrad <mlsemrad753@gmail.com>
2021-03-01 11:03:10 -0600

    finished enemies exercise

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,56 @@
-// Put your code here
+let enemies = [
+    {
+        firstName: "Joshua",
+        lastName: "Flowers",
+        offenses: [
+            "Being a jerk to me in elementary school",
+            "Not being nice to me in elementary school"
+        ],
+        isReallyHated: true
+    },
+    {
+        firstName: "Darth",
+        lastName: "Vader",
+        offenses: [
+            "Cut off Luke's hand",
+            "Murdered all those kids",
+            "Unkind management practices"
+        ],
+        isReallyHated: false
+    },
+    {
+        firstName: "Betty",
+        lastName: "Rudelady",
+        offenses: [
+            "Phone calls in the theater",
+            "Phone calls on the bus",
+            "Phone calls in line at the grocery store",
+            "Poor conversationalist"
+        ],
+        isReallyHated: true
+    },
+    {
+        firstName: "Leon",
+        lastName: "Peck",
+        offenses: [
+            "Keeps giving me a hotplate"
+        ],
+        isReallyHated: false
+    }
+];
+
+const enemiesList = (enemies) => {
+    const mappedEnemies = enemies.map(e => {
+        if (e.isReallyHated === true) {
+            console.log(`${e.firstName} ${e.lastName} (Really, really dislike!)`)
+        }
+        else {
+            console.log(`${e.firstName} ${e.lastName}`)
+        }
+    })
+    return mappedEnemies
+};
+
+console.log("My Enemies List!")
+console.log("-------------");;
+enemiesList(enemies);
